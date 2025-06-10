c4bf4d94c99a16410c3d1b93df488cea23ae31b8
Sarah-0911 <sarah.tabet69@gmail.com>
2025-06-10 15:00:16 +0200

    chore: remove Contentful integration

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,7 +2,5 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly CONTENTFUL_SPACE_ID: string
-  readonly CONTENTFUL_DELIVERY_TOKEN: string
-  readonly CONTENTFUL_PREVIEW_TOKEN: string
+
 }

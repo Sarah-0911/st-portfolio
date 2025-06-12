a9d0452f35ff0b30ed63876b998d81a149415b5a
Sarah-0911 <sarah.tabet69@gmail.com>
2025-06-12 16:38:10 +0200

    WIP: tooltip design to setup

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -61,3 +61,5 @@ export default function Tooltip({ children }) {
     </div>
   );
 }
+
+// "absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-xs opacity-0 tooltip dark:bg-gray-700"
